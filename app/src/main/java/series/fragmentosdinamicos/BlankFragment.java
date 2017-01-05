dea05e92f84a6104e5b519050c55c1c2263d5fdb
BrendaDilschneider <brenda_dilschneider@hotmail.com>
2017-01-05 10:57:13 -0300

    Comunicando fragment con activity

--- app/src/main/java/series/fragmentosdinamicos/BlankFragment.java
+++ app/src/main/java/series/fragmentosdinamicos/BlankFragment.java
@@ -1,9 +1,12 @@
 package series.fragmentosdinamicos;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +20,13 @@ import android.widget.Toast;
  * {@link BlankFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class BlankFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
     Button botonfragment;
+    Activity activity;
 
     public BlankFragment() {
         // Required empty public constructor
@@ -38,7 +43,8 @@ public class BlankFragment extends Fragment {
 
         botonfragment.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(getContext(),"Texto en el fragment",Toast.LENGTH_LONG);
+                activity= getActivity();
+                Toast.makeText(activity,"Texto en el fragment",Toast.LENGTH_LONG);
             }
         });
 
--- app/src/main/java/series/fragmentosdinamicos/MainActivity.java
+++ app/src/main/java/series/fragmentosdinamicos/MainActivity.java
@@ -1,11 +1,16 @@
 package series.fragmentosdinamicos;
 
+import android.annotation.TargetApi;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, BlankFragment.OnFragmentInteractionListener{
 
     Button boton;
 
@@ -18,8 +23,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         boton.setOnClickListener(this);
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     public void onClick(View v) {
+        //Paso 1: obtengo la instancia del administrador de fragmentos
+        FragmentManager fragmentManager= getFragmentManager();
+        //Paso 2: crear una nueva transacción
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        //Paso 3: crear nuevo fragmento y añadirlo
+        BlankFragment fragment = new BlankFragment();
+        transaction.add(R.id.layoutprincipal,fragment);
+        //Paso 4: confirmar el cambio
+        transaction.commit();
+    }
+
+    //Funcion de comunicacion actividad-fragmento
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
